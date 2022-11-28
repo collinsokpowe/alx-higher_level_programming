3043d566f1167c823abbdee73ce9c59c6caa51e3
collinsokpowe <collinsokpowe@gmail.com>
2022-11-28 13:19:54 -0800

    new header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,9 +1,13 @@
-fndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
 
-
+/**
+ * struct listint_s - A singly linked list
+ * @n: Integer
+ * @next: A pointer to the next node
+ */
 typedef struct listint_s
 {
 	int n;
@@ -11,8 +15,11 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+
 listint_t *add_nodeint(listint_t **head, const int n);
+
 void free_listint(listint_t *head);
+
 int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif
